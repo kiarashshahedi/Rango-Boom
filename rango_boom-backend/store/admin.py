ce20af4189e426663e8f3022d98879c6c9063c5d
Kiarash <shahedikiarash@gmail.com>
2025-01-15 17:39:46 +0330

    fix admin panel for Categories showing and sort them

--- rango_boom-backend/store/admin.py
+++ rango_boom-backend/store/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import Category, Product
 from django.utils.safestring import mark_safe
+from mptt.admin import DraggableMPTTAdmin
+from django.utils.html import format_html  
 
 
 class CategoryAdmin(admin.ModelAdmin):
@@ -12,10 +14,16 @@ class CategoryAdmin(admin.ModelAdmin):
 
     def icon_preview(self, obj):
         if obj.icon:
-            return mark_safe(f'<img src="{obj.icon.url}" style="width: 50px; height: 50px;" />')
+            return format_html(f'<img src="{obj.icon.url}" style="width: 50px; height: 50px;" />')
         return "No Icon"
 
     icon_preview.short_description = "Icon Preview"
+
+    def get_queryset(self, request):
+        """Customize queryset to order categories hierarchically."""
+        queryset = super().get_queryset(request)
+        return queryset.order_by('parent_category__id', 'name')
+
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['id', 'name', 'price', 'stock', 'category', 'created_at']
--- rango_boom-backend/store/models.py
+++ rango_boom-backend/store/models.py
@@ -1,14 +1,20 @@
 from django.db import models
 
-
 class Category(models.Model):
     name = models.CharField(max_length=255, unique=True)
-    parent_category = models.ForeignKey(
-        'self', on_delete=models.SET_NULL, null=True, blank=True, related_name='children'
-    )
     icon = models.ImageField(upload_to='category_icons/', blank=True, null=True)
 
+    parent_category = models.ForeignKey(
+        'self',
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name='children'
+    )
 
+    class Meta:
+        ordering = ['name']
+        
     def __str__(self):
         return self.name
 
